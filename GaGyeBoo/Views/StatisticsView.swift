faf780b49f6ba7c162e1893b2561a9bd52a79e44
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-06-04 23:39:59 +0900

    Add: add DGCharts Library && Design: StatisticsView

--- GaGyeBoo/Views/StatisticsView.swift
+++ GaGyeBoo/Views/StatisticsView.swift
@@ -1,6 +1,15 @@
 import UIKit
+import Charts
+import DGCharts
 
 class StatisticsView: UIView {
+    
+    var segmentedControl: UISegmentedControl!
+    var barChartView: BarChartView!
+    
+    var monthData: [String] = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
+    var incomeData: [Double] = [1000, 1200, 1300, 1100, 1050, 1150, 1250, 1230, 1280, 1150, 1200, 1250]
+    var expenseData: [Double] = [800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -13,15 +22,54 @@ class StatisticsView: UIView {
     }
     
     private func setupView() {
-        backgroundColor = .black
-        let label = UILabel()
-        label.text = "통계"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(label)
+        backgroundColor = .white
+        
+        segmentedControl = UISegmentedControl(items: ["수입", "지출"])
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        addSubview(segmentedControl)
+        
+        barChartView = BarChartView()
+        barChartView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(barChartView)
         
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+            segmentedControl.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            segmentedControl.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            segmentedControl.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            segmentedControl.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            barChartView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            barChartView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            barChartView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.8),
+            barChartView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5)
         ])
+        
+        updateBarChartData()
+    }
+    
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        updateBarChartData()
+    }
+    
+    private func updateBarChartData() {
+        var data: [Double] = []
+        let selectedIndex = segmentedControl.selectedSegmentIndex
+        var label = ""
+        
+        if selectedIndex == 0 {
+            data = incomeData
+            label = "수입"
+        } else {
+            data = expenseData
+            label = "지출"
+        }
+        
+        let entries = monthData.enumerated().map { BarChartDataEntry(x: Double($0.offset), y: data[$0.offset]) }
+        let dataSet = BarChartDataSet(entries: entries, label: label)
+        let chartData = BarChartData(dataSet: dataSet)
+        
+        barChartView.data = chartData
     }
 }
