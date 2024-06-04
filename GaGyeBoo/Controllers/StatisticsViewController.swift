1be0846f0840a63271617aa2d808e926b4cb8340
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-06-04 15:03:03 +0900

    InitialCommit: Connecting StatisticViewcontroller and statisticView

--- GaGyeBoo/Controllers/StatisticsViewController.swift
+++ GaGyeBoo/Controllers/StatisticsViewController.swift
@@ -1,16 +1,14 @@
-//
-//  StatsViewController.swift
-//  GaGyeBoo
-//
-//  Created by MadCow on 2024/6/4.
-//
-
 import UIKit
 
 class StatisticsViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = StatisticsView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
+        self.title = "통계"
     }
 }
--- GaGyeBoo/Views/StatisticsView.swift
+++ GaGyeBoo/Views/StatisticsView.swift
@@ -1,8 +1,27 @@
-//
-//  StatsView.swift
-//  GaGyeBoo
-//
-//  Created by MadCow on 2024/6/4.
-//
+import UIKit
 
-import Foundation
+class StatisticsView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        backgroundColor = .black
+        let label = UILabel()
+        label.text = "통계"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
