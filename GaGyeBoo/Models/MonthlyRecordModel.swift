82dc710ceace5783e796dde934743bc2a7cf6e9f
MadCow <lobasketve@naver.com>
2024-06-10 14:06:50 +0900

    MonthlyRecord 모델, Entity 추가

---
+++ GaGyeBoo/Models/MonthlyRecordModel.swift
@@ -0,0 +1,14 @@
+//
+//  MonthlyRecordModel.swift
+//  GaGyeBoo
+//
+//  Created by MadCow on 2024/6/10.
+//
+
+import Foundation
+
+struct MonthlyRecordModel {
+    let date: String
+    let totalSpend: Double
+    let mostSpendCategory: String
+}
