98f8f2a39bde94db74048377474559a994ad9ebc
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-06-12 17:05:35 +0900

    Refactoring: SettingsView && SettingViewController

---
+++ GaGyeBoo/Controllers/SettingsViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class SettingsViewController: UIViewController, SettingsViewDelegate {
+    private let settingsView = SettingsView()
+    
+    override func loadView() {
+        self.view = settingsView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingsView.delegate = self
+        title = "설정"
+        view.backgroundColor = .systemBackground
+    }
+    
+    func showBudgetSettingViewController() {
+        let budgetSettingVC = BudgetSettingViewController()
+        navigationController?.pushViewController(budgetSettingVC, animated: true)
+    }
+    
+    func showRecurringExpenseSettingViewController() {
+        let recurringExpenseSettingVC = RecurringExpenseSettingViewController()
+        navigationController?.pushViewController(recurringExpenseSettingVC, animated: true)
+    }
+    
+    func showInquiryViewController() {
+        let inquiryVC = InquiryViewController()
+        navigationController?.pushViewController(inquiryVC, animated: true)
+    }
+}
