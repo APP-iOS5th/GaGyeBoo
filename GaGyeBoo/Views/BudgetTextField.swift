f45bb54ca35ec8922f553199bd64337a5d2b3e27
Jinx <jinxwift@gmail.com>
2024-06-13 05:30:31 +0900

    240613 0530 updates

---
+++ GaGyeBoo/Views/BudgetTextField.swift
@@ -0,0 +1,30 @@
+//
+//  BudgetTextField.swift
+//  GaGyeBoo
+//
+//  Created by Jude Song on 6/13/24.
+//
+
+import UIKit
+
+class BudgetTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        keyboardType = .numberPad
+        addTarget(self, action: #selector(textFieldDidEndEditing), for: .editingDidEndOnExit)
+    }
+    
+    @objc private func textFieldDidEndEditing() {
+        resignFirstResponder()
+    }
+    
+}
